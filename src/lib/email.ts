d9abc4f7f9fbe8692d1d0262ef80dbea6f99e793
ni2k322 <nikolacv12345@gmail.com>
2026-03-03 23:49:22 +0100

    fix(emailjs): correct public key

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -4,7 +4,7 @@ import emailjs from '@emailjs/browser';
 // You can get them at https://dashboard.emailjs.com/admin/account
 const SERVICE_ID = "service_rh81nsq";
 const TEMPLATE_ID = "template_1vvvozo";
-const PUBLIC_KEY = "bl60hsPObEGwbyffFlLGB";
+const PUBLIC_KEY = "CS4ssbWT3AmmnKnx1";
 
 export const sendEmail = async (templateParams: Record<string, unknown>) => {
     try {
